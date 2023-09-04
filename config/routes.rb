bcb9d4c1327110ff4f19cd4f8628a69083537469
Daniil Kachur <kachur.daniil@gmail.com>
2023-09-04 17:25:33 +0400

    / -> /proposals

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "proposals#index"
+
   post "/proposal_form_validate/destination", to: "auto_check#validate_destination", as: :proposal_form_validate_destination
 end
